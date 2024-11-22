746380ebfbef7badeb1714300ce27f57145c7d7c
enoabasi <enoabasidream@gmail.com>
2024-11-22 09:33:24 +0100

    feat: add `neondb` + `drizzle-orm`
    - connection made in `drizzle.config.ts`

---
+++ database/db.ts
@@ -0,0 +1,8 @@
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+config({ path: ".env.local" }); // or .env.local
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle({ client: sql });
