ce54a403b3d1fc5f045b4e5835eed6c4f7fe27ae
enoabasi <enoabasidream@gmail.com>
2024-12-05 22:59:24 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
       <body
         className={cn(
           "antialiased",
