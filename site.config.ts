2ece11f805015399070d8a6b9bc2b4af7e5ac222
enoabasi <enoabasidream@gmail.com>
2024-12-05 23:03:00 +0100

    feat: define `site.config`

---
+++ site.config.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  application: "Noauth",
+  author: {
+    name: "Enoabasi Essien",
+    alias: "unorodeo",
+  },
+  socials: {
+    github: "https://github.com/unorodoe/noauth",
+  },
+};
