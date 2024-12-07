4e422e2bdcd6631d92791816aa68a5516d27329b
enoabasi <enoabasidream@gmail.com>
2024-12-07 11:51:02 +0100

    .

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
-import prisma from "@/lib/db";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
