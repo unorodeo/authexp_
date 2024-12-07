3d9f3516e2aefd6e980a52f6b1eb81bf56dd7685
enoabasi <enoabasidream@gmail.com>
2024-12-07 21:04:39 +0100

    refac: extend session to include `user.id`

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,16 +1,33 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
+
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/lib/auth.config";
 import prisma from "@/lib/db";
 
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: async ({ token, session }) => {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
   session: {
-    strategy: "jwt"
+    strategy: "jwt",
   },
   secret: process.env.AUTH_SECRET,
   pages: {
-    signIn: "/sign-in"
+    signIn: "/sign-in",
   },
   ...authConfig,
 });
