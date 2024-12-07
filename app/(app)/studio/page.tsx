819d8b5ab7801b81d3f8672bae646ac38889f67a
enoabasi <enoabasidream@gmail.com>
2024-12-07 16:09:44 +0100

    refac: show user data

--- app/(app)/studio/page.tsx
+++ app/(app)/studio/page.tsx
@@ -4,13 +4,17 @@ import { auth } from "@/lib/auth";
 export default async function Studio() {
   const session = await auth();
 
+  if (!session?.user) return null;
+
   return (
     <>
       <Section>
-        <h2 className="border-none">Welcome to your studio.</h2>
+        <h2 className="border-none">Welcome to your noauth.</h2>
         <p className="text-lg">
           Currently signed in as{" "}
-          <span className="font-medium text-primary">anon</span>
+          <span className="font-medium text-primary">
+            {session.user.email}
+          </span>
         </p>
       </Section>
       <Section>
