cf7468eccddf28f99393b6c42b5ec6fd667cb805
enoabasi <enoabasidream@gmail.com>
2024-12-07 11:50:35 +0100

    feat: add prisma adapter

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,8 @@
+import prisma from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [],
 });
