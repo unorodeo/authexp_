8aa0dda8047827c661d505a063be708c990f4811
enoabasi <enoabasidream@gmail.com>
2024-12-07 15:10:21 +0100

    refac: add redirect logic

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,31 @@
+import {
+  REDIRECT_SIGNIN,
+  protectedRoutes,
+  publicRoutes,
+} from "@/lib/auth.routes";
+
 import NextAuth from "next-auth";
 import authConfig from "@/lib/auth.config";
 
 const { auth: middleware } = NextAuth(authConfig);
 
 export default middleware((req) => {
-  const isLoggedIn = !!req.auth;
-  console.log("IS_LOGGED_IN:", isLoggedIn, "ON_ROUTE:", req.nextUrl.pathname);
+  const isAuthed = req.auth;
+
+  if (!isAuthed && protectedRoutes.includes(req.nextUrl.pathname)) {
+    const url = new URL("/sign-in", req.nextUrl.origin);
+    return Response.redirect(url);
+  }
+
+  if (isAuthed && publicRoutes.includes(req.nextUrl.pathname)) {
+    const url = new URL(REDIRECT_SIGNIN, req.nextUrl.origin);
+    return Response.redirect(url);
+  }
 });
 
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     * - /(marketing) routes
-     * - trpc endpoints
-     */
     "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-    // marketing routes
-    "/",
-    "/about",
-    "/contact",
-    "/colophon",
-    // trpc endpoints
     "/(api|trpc)(.*)",
   ],
 };
