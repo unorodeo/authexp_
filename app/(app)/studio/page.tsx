b8584b79f9dbf04ecbcb2afd289193e0469165d6
enoabasi <enoabasidream@gmail.com>
2024-12-07 16:18:54 +0100

    refac: redirect to "/sign-in" if not authed

--- app/(app)/studio/page.tsx
+++ app/(app)/studio/page.tsx
@@ -1,10 +1,13 @@
 import { Section } from "@/components/ui/section";
 import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Studio() {
   const session = await auth();
 
-  if (!session?.user) return null;
+  if (!session?.user) {
+    redirect("/sign-in")
+  };
 
   return (
     <>
