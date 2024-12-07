de0e551a7694dca1c50afab16f1af96ba65fdf8a
enoabasi <enoabasidream@gmail.com>
2024-12-07 16:19:18 +0100

    feat: sign out buttons for client and server

---
+++ components/sign-out.server.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export const SignOut: React.FC = () => {
+  return (
+    <Button
+      variant={"ghost"}
+      size={"sm"}
+    >
+      Sign Out
+    </Button>
+  );
+};
