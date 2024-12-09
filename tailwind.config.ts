d664f8ffc9a3177a94cfb59364c9eeab0eac6486
enoabasi <enoabasidream@gmail.com>
2024-12-09 12:16:59 +0100

    chore: remove `inter` font, use only local fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-geist-sans)", "var(--font-inter)"],
+        sans: ["var(--font-geist-sans)"],
         mono: ["var(--font-geist-mono)", "Consolas"],
       },
       borderRadius: {
