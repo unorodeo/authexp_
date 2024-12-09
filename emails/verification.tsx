e24cd8b9367449dbb55aa8987949d9f965dcfba2
enoabasi <enoabasidream@gmail.com>
2024-12-09 12:17:19 +0100

    refac: convert email to use tailwindcss

--- emails/verification.tsx
+++ emails/verification.tsx
@@ -5,6 +5,7 @@ import {
   Hr,
   Html,
   Preview,
+  Tailwind,
   Text,
 } from "@react-email/components";
 
@@ -17,21 +18,31 @@ interface Props {
 
 export const Verification: React.FC<Props> = ({ email, token }) => {
   return (
-    <Html lang="en">
-      <Preview>Your verification code for Norauth</Preview>
-      <Body>
-        <Container>
-          <Heading>Your login code for Norauth</Heading>
-          <Text>
-            This code will only be valid for the next 30 minutes. If the link
-            does not work, you can generate a new verification code by logging
-            in.
-          </Text>
-          <code>{token}</code>
-          <Hr />
-          <Text>Thank you for joining Norauth &mdash; {email}</Text>
-        </Container>
-      </Body>
-    </Html>
+    <Tailwind>
+      <Html lang="en">
+        <Preview>Verification code for Norauth</Preview>
+        <Body>
+          <Container>
+            <Heading>Verification code for Norauth</Heading>
+            <Text className="text-[16px] leading-7">
+              This code will only be valid for the next 30 minutes. If the link
+              does not work, you can generate a new verification code by logging
+              in.
+            </Text>
+            <code className="bg-neutral-100 border-[8px] border-blue-700 px-[12px] py-[10px] rounded-[8px] text-[24px] font-bold w-[100%] text-neutral-900">
+              {token}
+              672233
+            </code>
+            <Hr className="mt-[24px]"/>
+            <Text className="text-neutral-500 text-[14px] font-medium">
+              Thank you for joining Norauth &mdash;
+              <span className="text-blue-500 ms-[4px]">{email}enoabasidream@gmail.com</span>
+            </Text>
+          </Container>
+        </Body>
+      </Html>
+    </Tailwind>
   );
 };
+
+export default Verification;
