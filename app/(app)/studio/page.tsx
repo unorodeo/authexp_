6544e83b3dc71626c8fe712506a0897293155a44
enoabasi <enoabasidream@gmail.com>
2024-12-09 07:17:25 +0100

    refac: add metadata

--- app/(app)/studio/page.tsx
+++ app/(app)/studio/page.tsx
@@ -1,13 +1,19 @@
+import type { Metadata } from "next";
 import { Section } from "@/components/ui/section";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Studio",
+  description: "Noauth Studio"
+};
 
 export default async function Studio() {
   const session = await auth();
 
   if (!session?.user) {
-    redirect("/sign-in")
-  };
+    redirect("/sign-in");
+  }
 
   return (
     <>
@@ -15,9 +21,7 @@ export default async function Studio() {
         <h2 className="border-none">Welcome to your noauth.</h2>
         <p className="text-lg">
           Currently signed in as{" "}
-          <span className="font-medium text-primary">
-            {session.user.email}
-          </span>
+          <span className="font-medium text-primary">{session.user.email}</span>
         </p>
       </Section>
       <Section>
