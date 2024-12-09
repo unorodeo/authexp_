a9c8a466c1886dd5ba0d7c50b9badc69a9c35ee5
enoabasi <enoabasidream@gmail.com>
2024-12-09 14:53:01 +0100

    Update verification.tsx

--- emails/verification.tsx
+++ emails/verification.tsx
@@ -23,20 +23,19 @@ export const Verification: React.FC<Props> = ({ email, token }) => {
         <Preview>Verification code for Norauth</Preview>
         <Body>
           <Container>
-            <Heading>Verification code for Norauth</Heading>
-            <Text className="text-[16px] leading-7">
+            <Heading className="text-neutral-900">Verification code for Norauth</Heading>
+            <Text className="text-[16px] leading-7 text-neutral-900">
               This code will only be valid for the next 30 minutes. If the link
               does not work, you can generate a new verification code by logging
               in.
             </Text>
             <code className="bg-neutral-100 border-[8px] border-blue-700 px-[12px] py-[10px] rounded-[8px] text-[24px] font-bold w-[100%] text-neutral-900">
               {token}
-              672233
             </code>
             <Hr className="mt-[24px]"/>
             <Text className="text-neutral-500 text-[14px] font-medium">
               Thank you for joining Norauth &mdash;
-              <span className="text-blue-500 ms-[4px]">{email}enoabasidream@gmail.com</span>
+              <span className="text-blue-500 ms-[4px]">{email}</span>
             </Text>
           </Container>
         </Body>
