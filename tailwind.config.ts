d415d147193b13470a9d929bd56041944f2e652f
enoabasi <enoabasidream@gmail.com>
2024-12-09 09:28:44 +0100

    chore: move tailwind animations into `extend`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -65,15 +65,15 @@ const config: Config = {
           "5": "hsl(var(--chart-5))",
         },
       },
-    },
-    keyframes: {
-      "caret-blink": {
-        "0%,70%,100%": { opacity: "1" },
-        "20%,50%": { opacity: "0" },
+      keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
       },
-    },
-    animation: {
-      "caret-blink": "caret-blink 1.25s ease-out infinite",
+      animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
